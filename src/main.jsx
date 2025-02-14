924acc65c5a8bfe811ad14ab9ebd71c5ee0ec177
RianOntu <rianontu13@gmail.com>
2025-02-14 17:35:06 +0600

    set up react router and converted html to jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BookList from "./components/BookList.jsx";
+import AddBook from "./components/AddBook.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BookList />,
+  },
+  {
+    path:"/add-book",
+    element:<AddBook/>
+  }
+  
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
